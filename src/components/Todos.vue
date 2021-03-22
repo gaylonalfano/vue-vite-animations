2b6cf7ec2eb93a6c783c126f049bce706e86317b
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-22 11:32:13 +0800

    Learned about Transition move class for sliding and shifting. Learned about appear attribute that applies to Transition component for the initial render of a page load. Also learned how to transition between conditional elements eg that use v-if or v-else to make the swap switch smoother. We wrap all conditional elements with a single transition component and use the mode=out-in or in-out to control the order of how they swap.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,14 +6,17 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <ul>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </ul>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+    <!-- Wrap both conditionals with a Transition component to switch -->
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -89,5 +92,49 @@ input {
 }
 .todos li:hover {
   cursor: pointer;
+}
+
+.list-enter-from {
+  opacity: 0;
+  transform: scale(0.6);
+}
+.list-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+.list-enter-active {
+  transition: all 0.4s ease;
+}
+.list-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+.list-leave-to {
+  opacity: 0;
+  transform: scale(0.6);
+}
+.list-leave-active {
+  transition: all 0.4s ease;
+  /* Remove item from flow so remaining shift/slide up */
+  position: absolute;
+}
+.list-move {
+  transition: all 0.3s ease;
+}
+
+.switch-enter-from,
+.switch-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+/* Again, don't need these as it's browser defaults
+.switch-enter-to,
+.switch-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+} */
+.switch-enter-active,
+.switch-leave-active {
+  transition: all 0.3s ease;
 }
 </style>
