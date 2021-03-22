81293eaf8ffe39eaae15cc62ffb4af7dfee302a7
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-22 13:59:43 +0800

    Learned about Transition JavaScript Hooks that allow youu to run JS at different parts of the transition. You also have access to the element the transition is being applied to so you can change the el.style.color or whatever you'd like to do using JavaScript. You add the hooks to the transition element and then you create the handler functions inside the setup() method so you can access them. In order to apply the leave hooks, you must have the elemeent leave the DOM so you can use a v-if on the element to add/remove from the DOM.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition
+      appear
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
       officia possimus delectus inventore quod quisquam culpa voluptas iusto,
@@ -23,10 +34,55 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  setup() {},
+  name: "About",
+  setup() {
+    const showTitle = ref<boolean>(true);
+
+    // Enter hooks
+    function beforeEnter(el: HTMLTitleElement) {
+      console.log("beforeEnter", el);
+    }
+    function enter(el: HTMLTitleElement) {
+      console.log("enter", el);
+    }
+    function afterEnter(el: HTMLTitleElement) {
+      console.log("afterEnter", el);
+      el.style.color = "green";
+
+      setTimeout(() => (showTitle.value = false), 2000);
+
+      // ERROR Don't put console.log() inside setTimeout! Breaks!
+      // setTimeout(() => {
+      //   console.log("afterEnter: waiting for 2 seconds...");
+      //   (showTitle.value = false), 2000;
+      // });
+    }
+
+    // Leave hooks
+    function beforeLeave(el: HTMLTitleElement) {
+      console.log("beforeLeave", el);
+      el.style.color = "pink";
+    }
+    function leave(el: HTMLTitleElement) {
+      console.log("leave", el);
+    }
+    function afterLeave(el: HTMLTitleElement) {
+      console.log("afterLeave", el);
+    }
+
+    return {
+      showTitle,
+      beforeEnter,
+      enter,
+      afterEnter,
+      beforeLeave,
+      leave,
+      afterLeave,
+    };
+  },
 });
 </script>
 
@@ -34,5 +90,18 @@ export default defineComponent({
 .about {
   max-width: 600px;
   margin: 20px auto;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity 3s ease;
 }
 </style>
